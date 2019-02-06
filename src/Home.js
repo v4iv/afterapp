733277d952db04e56a323bd67da34d09b20df478
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-02-06 14:26:11 +0530

    On Click Render

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,38 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
+import fetch from "node-fetch";
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {stars: null, visible: false}
+    }
+
     static async getInitialProps({req, res, match, history, location, ...ctx}) {
         return {whatever: 'stuff'};
     }
 
+    handleClick = async () => {
+        const res = await fetch('https://api.github.com/repos/v4iv/gatsby-starter-business')
+        const json = await res.json()
+        this.setState({stars: json.stargazers_count, visible: true})
+    }
+
     render() {
         return (
-            <div>
+            <Fragment>
                 Hello, World!
                 <br/>
                 <Link to="/stars">Stars</Link>
-            </div>
+                <br/>
+                <br/>
+                <button onClick={this.handleClick}>Click Me</button>
+                <br/>
+                <br/>
+                {this.state.visible &&
+                <div>Stars: {this.state.stars}</div>
+                }
+            </Fragment>
         );
     }
 }
--- src/Stars.js
+++ src/Stars.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import fetch from 'node-fetch'
 
 const StarsPage = ({stars}) => {
     return (
-        <div>
+        <Fragment>
             Stars on Vaibhav's Repository:
             <br/>
             {stars}
-        </div>
+        </Fragment>
     )
 }
 
 StarsPage.getInitialProps = async ({req}) => {
     const res = await fetch('https://api.github.com/repos/v4iv/gatsby-starter-business')
     const json = await res.json()
-    console.log(json)
     return {stars: json.stargazers_count}
 }
 
